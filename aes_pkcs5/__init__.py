7724d3bcc1b16c954b5ef6bbbf7696201c024958
Laerte Pereira <laertefbk@gmail.com>
2022-07-12 23:38:46 -0300

    chore: Read Author from metadata

--- aes_pkcs5/__init__.py
+++ aes_pkcs5/__init__.py
@@ -1,8 +1,13 @@
 from sys import version_info
 
 if version_info >= (3, 8):
-    from importlib.metadata import version
+    from importlib.metadata import metadata
 else:
-    from importlib_metadata import version
+    import importlib_metadata as metadata
 
-__version__ = version("aes_pkcs5")
+_pkg_info = metadata("aes_pkcs5")
+
+__version__ = _pkg_info["Version"]
+__author__ = _pkg_info["Author"]
+
+__all__ = ("__version__", "__author__")
