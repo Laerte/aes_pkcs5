8dbd0fa465068810a99baa2a445f67060979ca0a
Laerte Pereira <laertefbk@gmail.com>
2022-07-12 23:06:46 -0300

    refactor: Use setup.cfg

--- aes_pkcs5/__init__.py
+++ aes_pkcs5/__init__.py
@@ -1,1 +1,8 @@
-__version__ = "0.1.1"
+from sys import version_info
+
+if version_info >= (3, 8):
+    from importlib.metadata import version
+else:
+    from importlib_metadata import version
+
+__version__ = version("aes_pkcs5")
