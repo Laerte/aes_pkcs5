7357306f8054aa2a4c7fd1fb5f112ae6d5dd2a30
Laerte Pereira <laertefbk@gmail.com>
2022-11-10 08:26:52 -0300

    refactor(packaging): Use setup.cfg

--- aes_pkcs5/__init__.py
+++ aes_pkcs5/__init__.py
@@ -1,1 +1,13 @@
-__version__ = "1.0.0"
+from sys import version_info
+
+if version_info >= (3, 8):
+    from importlib.metadata import metadata
+else:
+    from importlib_metadata import metadata
+
+_pkg_info = metadata("aes_pkcs5")
+
+__version__ = _pkg_info["Version"]
+__author__ = _pkg_info["Author"]
+
+__all__ = ("__version__", "__author__")
--- setup.py
+++ setup.py
@@ -1,9 +1,7 @@
 from os import system
 
-from setuptools import find_packages, setup
+from setuptools import setup
 from setuptools.command.develop import develop
-
-from aes_pkcs5 import __version__
 
 
 class CustomDevelopCommand(develop):
@@ -14,42 +12,4 @@ class CustomDevelopCommand(develop):
         system("pre-commit install")
 
 
-requires = ["cryptography >= 38.0.1"]
-
-extras = {}
-
-with open("README.rst") as f:
-    long_description = f.read()
-
-setup(
-    name="aes_pkcs5",
-    version=__version__,
-    description="Implementation of AES with CBC/ECB mode and padding scheme PKCS5",
-    long_description=long_description,
-    author="Laerte Pereira",
-    author_email="hi@laerte.dev",
-    license="BSD",
-    url="https://github.com/Laerte/aes_pkcs5",
-    packages=find_packages(include=["aes_pkcs5*"], exclude=["tests.*"]),
-    classifiers=[
-        "Intended Audience :: Developers",
-        "Development Status :: 5 - Production/Stable",
-        "Operating System :: OS Independent",
-        "License :: OSI Approved :: BSD License",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
-    python_requires=">=3.7",
-    include_package_data=True,
-    install_requires=requires,
-    extras_require=extras,
-    cmdclass={"develop": CustomDevelopCommand},
-)
+setup(cmdclass={"develop": CustomDevelopCommand})
